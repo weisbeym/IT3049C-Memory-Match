32b26a8c862fb1d0e222e7a8377ef6c974abc87d
Yisroel Weisberg <weisbeym@mail.uc.edu>
2018-07-01 17:01:11 -0400

    made basic game need to test

--- game.js
+++ game.js
@@ -1,108 +1,140 @@
 "use strict";
 
-const gameboard = document.querySelector("#gameboard");
-
-// Placeholder for the first selected tile of each turn
-let selectedTile = null;
-
-// Global value to maintain number of turns
-let turnCount = 0;
-
-/**
- * 1d array containing objects that represent the game board state.
- * For this I would recommend using an object to represent each tile:
- *
- * {
- *   image: "", // Path to the image file
- *   matched: false // Start with value "false" then change to true once matched
- * }
- *
- * NOTE: Using this method will always have the tiles in the same position. If you want to
- * take an extra challenge, you could create "base" tiles, then randomly assign their position
- * in the boardState. This is not a requirement for the assignment, but could be cool to try out
- */
-const boardState = [];
-
-function selectTile() {
-  // In this function, "this" represent the clicked on "div" element
-
-  // TODO: Get the tile represented in our boardState array
-  // const tile =
-
-  // Already matched
-  if (tile.matched) {
-    return;
-  }
-
-  // TODO: Remove the "hidden" class on the img child of this tile element
-
-  // If an unmatched tile is already selected
-  if (selectedTile) {
-    // TODO: Update turnCount
-
-    /*
-     * TODO: Check if the selectedTile and this tile have the same image
-     * 
-     * If they do, then leave them "revealed", update the "matched" property for
-     * both tiles in the "boardState", and check to see if the game is over (meaning
-     * all tiles are matched)
-     * 
-     * If they do not match, then set them both back to hidden
-     * 
-     */
-
-    // TIP: In both cases, you'll want to clear out the "selectedTile" holder as this is the end of our turn
-
-    return;
-  }
-
-  // TODO: Set "selectedTile" holder with current tile
-}
+let countDown;
+const timeLoss;
+const scoreIncrementer;
+const flippedCards;
+const score = document.getElementsByClassName('score')[0];
+const timer = document.getElementsByClassName('timer')[0];
+const gameOver = document.getElementsByClassName('game_over')[0];
+const startScreen = document.querySelector(".startScreen");
+const startButton = document.querySelector("#start");
+const gameboard = document.querySelector(".gameboard");
 
 function drawBoard() {
-  // Use this function to dynamically generate the game board
-  /**
-   * I would recommend using a for-loop and in each
-   * iteration, create the element and append it to gameboard element referenced
-   * from line 3
-   */
-  /**
-   * An implementation might look like this:
-   *
-   * for (let i = 0; i < boardState.length; i += 1) {
-   *   // TODO: Create tile "div" and add the "tile" class (see style.css)
-   *
-   *   // TODO: Set a retrievable property on the div to reference in selectTile function
-   *   // TIP: I would look into the setAttribute method and using "data-id" property
-   *
-   *   // TODO: Create "img" with the source being the current boardState tile "image" property
-   *
-   *   // TODO: Append the img to the div tile
-   *
-   *   // TODO: Append the div tile to the gameboard
-   * }
-   *
-   */
-  /**
-   * At the end of this function our gameboard element will now contain dynamically
-   * generated HTML elements that look something like this:
-   *
-   * <div class="tile" onclick="OurSelectTileFunction" data-id="WhateverIndexThisWasCreatedIn">
-   *   <img src="TheSourceFromOurBoardState" />
-   * </div>
-   */
+  // draw an empty board on the press of the start button or the reset button.
+  const card = document.getElementsByClassName('card');
+  const images = ["url('images/zap.png')", "url('images/pow.png')", "url('images/kapow.png')"];
+
+  // reset/create empty game fields 
+  timeLoss = 119;
+  scoreIncrementer = 0;
+  flippedCards = [];
+
+  //display the board and hide the endgame/start screens
+  // check if game over is hidden if not make it so
+  if(gameOver.classList.contains("hidden")) {
+    return;
+  } else {
+    gameOver.classList.add("hidden");
+  }
+  
+  // make the start screen hidden
+  startScreen.classList.add("hidden");
+
+  //make the board visible 
+  gameboard.classList.remove("hidden");
+
+  // shuffle the order of the images
+  shuffle(images);
+
+  //place the images by the cards 
+  for(let i = 0; i < card.length; i++) {
+    if(card[i].classList.contains("flipped")) {
+      card[i].classList.toggle("flipped");
+    }
+    card[i].querySelector('.back').style.backgroundImage = pics[i];
+    card[i].addEventListener('click', flip);
+  }
+
+  score.innerText = "00";
+  startTimer();
 }
 
-// This is an optional step if you want to take on the extra challenge
-// function reset() {
-// TODO: Clear the gameboard element
+function flip() {
+  if(!this.classList.contains("flipped") && flippedCards < 2) {
+    this.classList.toggle("flipped");
+    flippedCards.push(this);
 
-// TODO: Reset all the boardState tiles to have "false" for their matched properties
-// TODO: Clear the selectedTile holder
-// TODO: Reset turnCount
+    if(flippedCards.length === 2) {
+      checkMatch();
+    }
+  }
+}
 
-// TODO: Draw the game board again since we cleared out the element
-// }
+function checkMatch() {
+  if(flippedCards[0].querySelector(".back").style.backgroundImage === flippedCards[1].querySelector(".back").style.backgroundImage) {
+    flippedCards =[];
+    score.innerText = "0" + ++scoreIncrementer;
+  } else {
+    setTimeout(flipBack, 1500);
+  }
+}
 
-// Immediately draw the game board when this file is loaded and parsed by the browser
+function flipBack() {
+  flippedCards[0].classList.toggle("flipped");
+  flippedCards[1].classList.toggle("flipped");
+  flippedCards = [];
+}
+
+function startTimer() {
+  timer.innerText = '2:00';
+  countDown = setInterval(decrementTime, 2000);
+}
+
+function decrementTime() {
+  if(timeLoss === 0) {
+    timer.innerText = "0:0" +  timeLoss;
+    clearInterval(countDown);
+    finalize();
+  }
+
+  if(timeLoss < 10) {
+    timer.innerText = "0:0" + timeLoss;
+  }
+
+  if(timeLoss >= 10) {
+    timer.innerText = "0:" + timeLoss;
+  }
+
+  if (scoreIncrementer === 8){
+    clearInterval(countDown);
+    finalize();
+  }
+
+  timeLoss--;
+}
+
+function finalize() {
+  const restart = document.getElementsByTagName("button")[0];
+  restart.addEventListener("click", drawBoard);
+
+  gameOver.style.display = "flex";
+
+  if (scoreIncrementer === 8) {
+    gameOver.querySelector('h1').innerText = 'you win';
+  }
+  else {
+    gameOver.querySelector('h1').innerText = 'you lose';
+  }
+
+  gameOver.querySelector('.final_score').innerText = 'score: ' + scoreIncrementer;
+  gameOver.querySelector('.time').innerText = 'time left: ' + timeLoss + ' sec.';
+}
+
+function shuffle(array) {
+  for(let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+startButton.addEventListener("click", function() {
+  startScreen.classList.add("hidden");
+  drawBoard();
+})
+
 drawBoard();
